6c1090e53aa9735e894c31d263185da1698f4ba6
Nile Young <nilevy03@gmail.com>
2024-11-06 13:08:39 -0700

    integrated factorial() into e()

--- code.js
+++ code.js
@@ -1,31 +1,20 @@
-function factorial(n) {
+function e(n) {
+  if (n < 0) return 0;
+    
   facts = [];
 
   facts[0] = 1;
+  sum = 1;
 
   for (let i = 1; i <= n; i++) {
     facts[i] = i * facts[i - 1];
   }
-
-  return facts;
-}
-
-// mymap = factorial(5);
-// console.log(mymap[2]);
-
-function e(n) {
-  if (n < 0) return 0;
-
-  ourmap = factorial(n)
-  sum = 1;
+  
 
   for (let i = 1; i <= n; i++) {
-    sum += 1.0 / ourmap[i];
-    // console.log(sum);
+    sum += 1.0 / facts[i];
+    
   }
 
   return sum;
 }
-
-console.log(e(10));
-console.log(factorial(10));
