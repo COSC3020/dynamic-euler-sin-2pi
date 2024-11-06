ca7f502e9d7ca48f07b11d74117c984035c15fcb
Nile Young <nilevy03@gmail.com>
2024-11-06 16:30:56 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,17 +1,13 @@
 function e(n) {
   if (n < 0) return 0;
-    
+
   facts = [];
+
   facts[0] = 1;
-  
   sum = 1;
 
   for (let i = 1; i <= n; i++) {
     facts[i] = i * facts[i - 1];
-  }
-  
-
-  for (let i = 1; i <= n; i++) {
     sum += 1.0 / facts[i];
   }
 
